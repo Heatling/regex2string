8ad5dcf37517d668ba6e41d688ef904db6803ae4
Emad Maroun <heatling@outlook.com>
2016-09-03 21:28:42 +0200

    Added a feature

--- src/test/java/feature/Features.java
+++ src/test/java/feature/Features.java
@@ -9,6 +9,7 @@ import dk.brics.automaton.State;
 import dk.heatless.regex2string.Conditions;
 import dk.heatless.regex2string.GenerationState;
 import dk.heatless.regex2string.Generators;
+import dk.heatless.regex2string.generators.MinimalDigitGenerator;
 import dk.heatless.regex2string.generators.RepeatGenerator;
 import dk.heatless.regex2string.generators.RuleSetGenerator;
 import dk.heatless.regex2string.generators.StaticStringGenerator;
@@ -51,5 +52,17 @@ public class Features {
 		GenerationState gen = StateOperations.getInitialGenerationState(new RegExp("(\\+|-) 1234 (\\+|-)"));
 		
 		assertEquals(rG.generate(gen), "- 1234 +");
+	}
+	
+	@Test
+	public void t4(){
+		RuleSetGenerator rules = new RuleSetGenerator();
+		rules.add(new PrefixRule(new StaticStringGenerator("-")));
+		rules.add(new PostfixRule(new StaticStringGenerator("23")));
+		PriorityGenerator pG = new PriorityGenerator(Conditions.NO_CONDITION, rules, new MinimalDigitGenerator());
+		RepeatGenerator rG = new RepeatGenerator(pG);
+		GenerationState gen = StateOperations.getInitialGenerationState(new RegExp("(\\+|-)[0-9]{10}"));
+		
+		assertEquals(rG.generate(gen), "-0000000023");
 	}
 }
